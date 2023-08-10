b989aa73f8a9ee02f50b08a362f7b13bff85c33e
yifengyou <842056007@qq.com>
2023-08-10 22:15:28 +0800

    remove generated code

--- 1.eaidk610-linux.sh
+++ 1.eaidk610-linux.sh
@@ -5,29 +5,38 @@ set -e
 export PATH=`realpath ../gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu/bin/`:$PATH
 echo $PATH
 
+CURRENTDIR=`pwd`
+echo "CURRENTDIR: ${CURRENTDIR}"
+BUILDDIR=${CURRENTDIR}-build
+echo "BUILDDIR: ${BUILDDIR}"
+
+
 echo    "******************************"
 echo    "*     Clean Kernel Config     *"
 echo    "******************************"
 make distclean
 echo "make distclean done! [$?]"
 
+if [ -d ${BUILDDIR} ]; then
+	rm -rf ${BUILDDIR}
+fi
+mkdir -p ${BUILDDIR}
 
 echo    "******************************"
 echo    "*     Make Kernel Config     *"
 echo    "******************************"
-make ARCH=arm64  CROSS_COMPILE="aarch64-none-linux-gnu-" rockchip_linux_defconfig
+make O=${BUILDDIR} ARCH=arm64 CROSS_COMPILE="aarch64-none-linux-gnu-" rockchip_linux_defconfig
 echo "make rk3399_linux_defconfig done! [$?]"
 
 # fix yylloc redefine bug
-sed -i 's/^YYLTYPE yylloc;$/extern YYLTYPE yylloc;/g' scripts/dtc/dtc-lexer.lex.c
+# sed -i 's/^YYLTYPE yylloc;$/extern YYLTYPE yylloc;/g' scripts/dtc/dtc-lexer.lex.c
 
 echo    "******************************"
 echo    "*     Make AArch64 Kernel    *"
 echo    "******************************"
-make rk3399-eaidk-linux.img ARCH=arm64 CROSS_COMPILE="aarch64-none-linux-gnu-" -j `nproc` | tee log.eaidk610linux
+make O=${BUILDDIR} rk3399-eaidk-linux.img ARCH=arm64 CROSS_COMPILE="aarch64-none-linux-gnu-" -j `nproc` | tee log.eaidk610linux
 echo "make rk3399-eaidk-linux.img done! [$?]"
 
-make modules ARCH=arm64 CROSS_COMPILE="aarch64-none-linux-gnu-" -j `nproc` | tee log.eaidk610linux
+make O=${BUILDDIR} modules ARCH=arm64 CROSS_COMPILE="aarch64-none-linux-gnu-" -j `nproc` | tee log.eaidk610linux
 
 echo "All done!"
-exit 0
